be2852560770506a27970f7a46382ba326f35757
nicorama <nzozol@gmail.com>
2014-02-08 21:00:08 +0100

    Adding empty tests

---
+++ src/test/java/io/robusta/rra/RepresentationTest.java
@@ -0,0 +1,122 @@
+package io.robusta.rra;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by  Nicolas Zozol for Robusta Code
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class RepresentationTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWithClass() throws Exception {
+
+    }
+
+    @Test
+    public void testHas() throws Exception {
+
+    }
+
+    @Test
+    public void testHasNotEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMissingKeys() throws Exception {
+
+    }
+
+    @Test
+    public void testSet() throws Exception {
+
+    }
+
+    @Test
+    public void testSetWithClass() throws Exception {
+
+    }
+
+    @Test
+    public void testGetValues() throws Exception {
+
+    }
+
+    @Test
+    public void testGetValuesWithClass() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testAddResource() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAll() throws Exception {
+
+    }
+
+    @Test
+    public void testMerge() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+
+    @Test
+    public void testFetch() throws Exception {
+
+    }
+
+    @Test
+    public void testCopy() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRepresentation() throws Exception {
+
+    }
+}
