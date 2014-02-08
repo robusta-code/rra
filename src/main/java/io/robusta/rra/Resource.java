1c2a0aa5eed92f7a05e8665e824265e156cc381a
nicorama <nzozol@gmail.com>
2014-02-08 13:52:14 +0100

    Added Apache 2 licence

--- src/main/java/io/robusta/rra/Resource.java
+++ src/main/java/io/robusta/rra/Resource.java
@@ -4,10 +4,19 @@ import java.util.Map;
 
 /**
  * Created by  Nicolas Zozol for Robusta Code
- * Released under Apache 2 Licence : https://www.apache.org/dev/apply-license.html
- * AS-IS With NO Guarantee, Use at your own risks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 public interface Resource<IdType> {
 
     /**
@@ -19,7 +28,7 @@ public interface Resource<IdType> {
      */
     public IdType getId();
 
-   
+
 
 
 
@@ -32,6 +41,7 @@ public interface Resource<IdType> {
      *  <house><id>3</id><name>Big House</name></house>
      * </houses>
      * Or : {houses:[{id:"1", name:"House 1"}, {id:"2", name:"Small House"}, {id:"3", name:"Big House"}]}
+     * @return resource prefix
      */
     public String getPrefix();
 
@@ -44,6 +54,7 @@ public interface Resource<IdType> {
      *  <house><id>3</id><name>Big House</name></house>
      * </houses>
      * Or : {houses:[{id:"1", name:"House 1"}, {id:"2", name:"Small House"}, {id:"3", name:"Big House"}]}
+     * @return resource list prefix
      */
     public String getListPrefix();
 
