cd3458400dd71f1c281d203aee4bc133958c9860
nicorama <nzozol@gmail.com>
2014-08-30 00:23:58 +0200

    Copyright management

--- src/main/java/io/robusta/rra/resource/ResourceCollectionWrapper.java
+++ src/main/java/io/robusta/rra/resource/ResourceCollectionWrapper.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2014. by Robusta Code and individual contributors
+ *  as indicated by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.robusta.rra.resource;
 
 import io.robusta.rra.Resource;
@@ -8,7 +31,7 @@ import java.util.List;
 
 /**
  * Add features to a Resource Collection without creating a new Collection
- * Created by dev on 29/08/14.
+ * @author Nicolas Zozol
  */
 public class ResourceCollectionWrapper<IdType, R extends Resource<IdType>> implements ResourceCollection<IdType, R> {
 
