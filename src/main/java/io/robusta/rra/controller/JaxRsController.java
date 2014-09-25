34f8a407adeb8daac70012646b29df2a364dc764
jean-marc-villatte <jean_marc.villatte@aliceadsl.fr>
2014-09-25 12:09:53 +0200

    merging branch Client

--- src/main/java/io/robusta/rra/controller/JaxRsController.java
+++ src/main/java/io/robusta/rra/controller/JaxRsController.java
@@ -1,14 +1,18 @@
 package io.robusta.rra.controller;
 
-import io.robusta.rra.Representation;
-import io.robusta.rra.Rra;
-import io.robusta.rra.controller.ControllerException;
+import io.robusta.rra.representation.Representation;
+import io.robusta.rra.representation.Rra;
 import io.robusta.rra.security.implementation.CodecException;
 import io.robusta.rra.security.implementation.CodecImpl;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.core.*;
 import java.util.List;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * Created by Stephanie Pitou on 12/09/14.
@@ -17,10 +21,14 @@ public class JaxRsController {
 
     public static Representation defaultRepresentation = Rra.defaultRepresentation;
 
-    @Context HttpHeaders httpHeader;
-    @Context UriInfo uriInfo;
-    @Context Response response;
-    @Context Request request;
+    @Context
+    HttpHeaders                  httpHeader;
+    @Context
+    UriInfo                      uriInfo;
+    @Context
+    Response                     response;
+    @Context
+    Request                      request;
 
     public HttpHeaders getHttpHeader() {
         return httpHeader;
@@ -35,24 +43,24 @@ public class JaxRsController {
     }
 
     public boolean isJsonApplication() {
-        List<String> type = getHeaders().get("content-type");
-        return (type.get(0) != null && type.get(0).equals("application/json"));
+        List<String> type = getHeaders().get( "content-type" );
+        return ( type.get( 0 ) != null && type.get( 0 ).equals( "application/json" ) );
     }
 
-    public boolean isSecure(UriInfo uriInfo) {
-        return uriInfo.getAbsolutePath().toString().contains("https");
+    public boolean isSecure( UriInfo uriInfo ) {
+        return uriInfo.getAbsolutePath().toString().contains( "https" );
     }
 
     public String[] getBasicAuthentification() {
         String[] values = new String[2];
-        List<String> authorization = getHeaders().get("authorization");
-        if (authorization.get(0) != null && authorization.get(0).startsWith("Basic")) {
-            String base64Credentials = authorization.get(0).substring("Basic".length()).trim();
+        List<String> authorization = getHeaders().get( "authorization" );
+        if ( authorization.get( 0 ) != null && authorization.get( 0 ).startsWith( "Basic" ) ) {
+            String base64Credentials = authorization.get( 0 ).substring( "Basic".length() ).trim();
             CodecImpl codecimpl = new CodecImpl();
             try {
-                values[0] = codecimpl.getUsername(base64Credentials);
-                values[1] = codecimpl.getPassword(base64Credentials);
-            } catch (CodecException e) {
+                values[0] = codecimpl.getUsername( base64Credentials );
+                values[1] = codecimpl.getPassword( base64Credentials );
+            } catch ( CodecException e ) {
                 e.printStackTrace();
             }
         }
@@ -60,56 +68,61 @@ public class JaxRsController {
     }
 
     public Response getBasicAuthentificationResponse() {
-        if (!isSecure(getUriInfo())) {
-            return response.status(401).entity("<a href='http://docs.oracle.com/javaee/5/tutorial/doc/bnbxw.html'>Establishing a Secure Connection Using SSL</a>").build();
+        if ( !isSecure( getUriInfo() ) ) {
+            return response
+                    .status( 401 )
+                    .entity(
+                            "<a href='http://docs.oracle.com/javaee/5/tutorial/doc/bnbxw.html'>Establishing a Secure Connection Using SSL</a>" )
+                    .build();
         } else
-            return response.status(200).entity("The authentification is secure !").build();
+            return response.status( 200 ).entity( "The authentification is secure !" ).build();
     }
 
-    protected void throwIfNull(Representation representation) throws ControllerException {
-        if (representation == null) {
-            throw new ControllerException("Representation is null");
+    protected void throwIfNull( Representation representation ) throws ControllerException {
+        if ( representation == null ) {
+            throw new ControllerException( "Representation is null" );
         }
     }
 
-    public Representation getRepresentation(String requestEntity) {
-        return defaultRepresentation.createNewRepresentation(getRequestEntity(requestEntity));
+    public Representation getRepresentation( String requestEntity ) {
+        return defaultRepresentation.createNewRepresentation( getRequestEntity( requestEntity ) );
     }
 
-    public boolean validate(String requestEntity, String... keys) {
-        Representation representation = getRepresentation(requestEntity);
-        throwIfNull(representation);
-        return representation.has(keys);
+    public boolean validate( String requestEntity, String... keys ) {
+        Representation representation = getRepresentation( requestEntity );
+        throwIfNull( representation );
+        return representation.has( keys );
     }
 
-    public Response validateResponse(String requestEntity, String... keys) {
-        if (!validate(requestEntity, keys)) {
-            return response.status(406).entity("Json representation is not valid !").build();
+    public Response validateResponse( String requestEntity, String... keys ) {
+        if ( !validate( requestEntity, keys ) ) {
+            return response.status( 406 ).entity( "Json representation is not valid !" ).build();
         } else
-            return response.status(200).entity(requestEntity).build();
+            return response.status( 200 ).entity( requestEntity ).build();
     }
 
-    public String getRequestEntity(String requestEntity) {
+    public String getRequestEntity( String requestEntity ) {
         return requestEntity;
     }
 
     public List<String> getUserAgent() {
-        return getHeaders().get("user-agent");
+        return getHeaders().get( "user-agent" );
     }
 
     public boolean isChrome() {
-        return getUserAgent().get(0).toUpperCase().contains("CHROME");
+        return getUserAgent().get( 0 ).toUpperCase().contains( "CHROME" );
     }
 
     public boolean isFirefox() {
-        return getUserAgent().get(0).toUpperCase().contains("FIREFOX");
+        return getUserAgent().get( 0 ).toUpperCase().contains( "FIREFOX" );
     }
 
     public boolean isTablet() {
-        return getUserAgent().get(0).toUpperCase().contains("TABLET") || getUserAgent().get(0).toUpperCase().contains("IPAD");
+        return getUserAgent().get( 0 ).toUpperCase().contains( "TABLET" )
+                || getUserAgent().get( 0 ).toUpperCase().contains( "IPAD" );
     }
 
     public boolean isMobile() {
-        return getUserAgent().get(0).toUpperCase().contains("MOBILE");
+        return getUserAgent().get( 0 ).toUpperCase().contains( "MOBILE" );
     }
 }
