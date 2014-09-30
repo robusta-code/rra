d44927f7e6a9b5c59221bd2fe43d8ce2a8b43fe6
stephanie-pitou <stephanie.pitou@gmail.com>
2014-09-30 16:41:24 +0200

    adding GsonJaxRSProvider implementation

--- src/main/java/io/robusta/rra/representation/implementation/GsonJaxRsProvider.java
+++ src/main/java/io/robusta/rra/representation/implementation/GsonJaxRsProvider.java
@@ -1,7 +1,33 @@
+/*
+ * Copyright (c) 2014. by Robusta Code and individual contributors
+ *  as indicated by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.robusta.rra.representation.implementation;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
@@ -14,78 +40,61 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 
 /**
- * @author Nicolas Zozol
- *
+ * Created by Stephanie Pitou on 15/09/14.
  */
 @Provider
-@Consumes( "application/json" )
-@Produces( "text/plain" )
+@Consumes("application/json")
+@Produces("application/json")
 public class GsonJaxRsProvider implements MessageBodyReader<GsonRepresentation>, MessageBodyWriter<GsonRepresentation> {
 
-    @Override
-    public boolean isReadable( Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType ) {
-        return aClass == GsonRepresentation.class;
-    }
+	private ObjectOutputStream oos;
 
-    @Override
-    public GsonRepresentation readFrom( Class<GsonRepresentation> gsonRepresentationClass, Type type,
-            Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> stringStringMultivaluedMap,
-            InputStream inputStream ) throws IOException, WebApplicationException {
-        try {
-            JAXBContext ctx = JAXBContext.newInstance( gsonRepresentationClass );
-            return (GsonRepresentation) ctx.createUnmarshaller().unmarshal( inputStream );
-        } catch ( JAXBException e ) {
-            e.printStackTrace();
-            throw new RuntimeException( e );
-        }
-    }
+	@Override
+	public boolean isReadable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+		return aClass == GsonRepresentation.class;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class,
-     * java.lang.reflect.Type, java.lang.annotation.Annotation[],
-     * javax.ws.rs.core.MediaType)
-     */
-    @Override
-    public boolean isWriteable( Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType ) {
-        return type == GsonRepresentation.class;
-    }
+	@Override
+	public GsonRepresentation readFrom(Class<GsonRepresentation> gsonRepresentationClass, Type type,
+			Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> stringStringMultivaluedMap,
+			InputStream inputStream) throws IOException, WebApplicationException {
+		return new GsonRepresentation(inputStream);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object,
-     * java.lang.Class, java.lang.reflect.Type,
-     * java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
-     */
-    @Override
-    public long getSize( GsonRepresentation representation, Class<?> aClass, Type type, Annotation[] annotations,
-            MediaType mediaType ) {
-        return -1;
-    }
+	@Override
+	public boolean isWriteable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+		return type == GsonRepresentation.class;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object,
-     * java.lang.Class, java.lang.reflect.Type,
-     * java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType,
-     * javax.ws.rs.core.MultivaluedMap, java.io.OutputStream)
-     */
-    @Override
-    public void writeTo( GsonRepresentation representation, Class<?> aClass, Type type, Annotation[] annotations,
-            MediaType mediaType, MultivaluedMap<String, Object> stringObjectMultivaluedMap, OutputStream outputStream )
-            throws IOException, WebApplicationException {
-        try {
-            JAXBContext ctx = JAXBContext.newInstance( aClass );
-            ctx.createMarshaller().marshal( representation, outputStream );
-        } catch ( JAXBException e ) {
-            throw new RuntimeException( e );
-        }
-    }
+	@Override
+	public long getSize(GsonRepresentation representation, Class<?> aClass, Type type, Annotation[] annotations,
+			MediaType mediaType) {
+		return -1;
+	}
+
+	@Override
+	public void writeTo(GsonRepresentation representation, Class<?> aClass, Type type, Annotation[] annotations,
+			MediaType mediaType, MultivaluedMap<String, Object> stringObjectMultivaluedMap, OutputStream outputStream)
+			throws IOException, WebApplicationException {
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(baos);
+
+		oos.writeObject(representation.toString());
+
+		oos.flush();
+		oos.close();
+	}
+
+	public void closeStream() {
+		try {
+			oos.flush();
+			oos.close();
+			System.out.println("Steam has been closed successfully !");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
--- src/test/java/io/robusta/rra/client/implementation/AbstractAsynchronousRestClientJDKTest.java
+++ src/test/java/io/robusta/rra/client/implementation/AbstractAsynchronousRestClientJDKTest.java
@@ -25,123 +25,123 @@ import org.junit.Test;
  */
 public class AbstractAsynchronousRestClientJDKTest {
 
-    JdkRestClient client;
+	JdkRestClient client;
 
-    public AbstractAsynchronousRestClientJDKTest() {
-    }
+	public AbstractAsynchronousRestClientJDKTest() {
+	}
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
+	// @BeforeClass
+	public static void setUpClass() throws Exception {
+	}
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
+	// @AfterClass
+	public static void tearDownClass() throws Exception {
+	}
 
-    @Before
-    public void setUp() {
-        client = new JdkRestClient( "http://localhost:8080/classify" );
-    }
+	// @Before
+	// public void setUp() {
+	// client = new JdkRestClient( "http://localhost:8080/classify" );
+	// }
 
-    @After
-    public void tearDown() {
-    }
+	// @After
+	public void tearDown() {
+	}
 
-    @Test
-    public void testSetApplicationUri() {
-    }
+	@Test
+	public void testSetApplicationUri() {
+	}
 
-    // @Test
-    public void testSetAuthorizationValue() throws Exception {
-        client.setAuthorizationValue( "James Bond" );
+	// @Test
+	public void testSetAuthorizationValue() throws Exception {
+		client.setAuthorizationValue("James Bond");
 
-        Callback cb = new SimpleCallback( client ) {
+		Callback cb = new SimpleCallback(client) {
 
-            @Override
-            public void onSuccess( InputStream inputStream ) {
+			@Override
+			public void onSuccess(InputStream inputStream) {
 
-                System.out.println( "success :" + new GsonRepresentation( inputStream ).toString() );
-            }
+				System.out.println("success :" + new GsonRepresentation(inputStream).toString());
+			}
 
-            @Override
-            public void onFailure( RestException ex ) {
-                ex.printStackTrace();
-                System.out.println( "fail : " + client.getHttpCode() );
-            }
+			@Override
+			public void onFailure(RestException ex) {
+				ex.printStackTrace();
+				System.out.println("fail : " + client.getHttpCode());
+			}
 
-            @Override
-            public void onComplete() {
-                System.out.println( "complete" );
-            }
-        };
+			@Override
+			public void onComplete() {
+				System.out.println("complete");
+			}
+		};
 
-        client.post( "spring", new GsonRepresentation( "{\"email\":\"email\",\"name\":\"name\"}" ), cb );
-        client.join();
-        System.out.println( "client.getHttpCode()=" + client.getHttpCode() );
-        assertTrue( client.getHttpCode() < 300 );
-        System.out.println( "finished" );
+		client.post("spring", new GsonRepresentation("{\"email\":\"email\",\"name\":\"name\"}"), cb);
+		client.join();
+		System.out.println("client.getHttpCode()=" + client.getHttpCode());
+		assertTrue(client.getHttpCode() < 300);
+		System.out.println("finished");
 
-    }
+	}
 
-    @Test
-    public void testSetContentType() {
-    }
+	@Test
+	public void testSetContentType() {
+	}
 
-    @Test
-    public void testSetRequestBody() {
-    }
+	@Test
+	public void testSetRequestBody() {
+	}
 
-    @Test
-    public void testExecuteGet() throws Exception {
-    }
+	@Test
+	public void testExecuteGet() throws Exception {
+	}
 
-    @Test
-    public void testExecuteMethod() {
-    }
+	@Test
+	public void testExecuteMethod() {
+	}
 
-    // @Test
-    public void testExecutePost() throws Exception {
+	// @Test
+	public void testExecutePost() throws Exception {
 
-        Callback cb = new SimpleCallback( client ) {
+		Callback cb = new SimpleCallback(client) {
 
-            @Override
-            public void onSuccess( InputStream inputStream ) {
+			@Override
+			public void onSuccess(InputStream inputStream) {
 
-                System.out.println( "success :" + new GsonRepresentation( inputStream ).toString() );
-            }
+				System.out.println("success :" + new GsonRepresentation(inputStream).toString());
+			}
 
-            @Override
-            public void onFailure( RestException ex ) {
-                ex.printStackTrace();
-                System.out.println( "fail : " + client.getHttpCode() );
-            }
+			@Override
+			public void onFailure(RestException ex) {
+				ex.printStackTrace();
+				System.out.println("fail : " + client.getHttpCode());
+			}
 
-            @Override
-            public void onComplete() {
-                System.out.println( "complete" );
-            }
-        };
+			@Override
+			public void onComplete() {
+				System.out.println("complete");
+			}
+		};
 
-        client.post( "spring", new GsonRepresentation( "{\"email\":\"email\",\"name\":\"name\"}" ), cb );
-        client.join();
-        System.out.println( "client.getHttpCode()=" + client.getHttpCode() );
-        assertTrue( client.getHttpCode() < 300 );
-        System.out.println( "finished" );
-    }
+		client.post("spring", new GsonRepresentation("{\"email\":\"email\",\"name\":\"name\"}"), cb);
+		client.join();
+		System.out.println("client.getHttpCode()=" + client.getHttpCode());
+		assertTrue(client.getHttpCode() < 300);
+		System.out.println("finished");
+	}
 
-    @Test
-    public void testExecutePut() throws Exception {
-    }
+	@Test
+	public void testExecutePut() throws Exception {
+	}
 
-    @Test
-    public void testExecuteDelete() throws Exception {
-    }
+	@Test
+	public void testExecuteDelete() throws Exception {
+	}
 
-    @Test
-    public void testGetLastStatusCode() {
-    }
+	@Test
+	public void testGetLastStatusCode() {
+	}
 
-    @Test
-    public void testGetLastResponse() {
-    }
+	@Test
+	public void testGetLastResponse() {
+	}
 }
