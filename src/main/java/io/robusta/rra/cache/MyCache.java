32cd0c2afb7ca2f29ee740cb95dd616ea0fe46d4
jean-marc-villatte <jean_marc.villatte@aliceadsl.fr>
2014-10-20 11:59:31 +0200

    cache completed

--- src/main/java/io/robusta/rra/cache/MyCache.java
+++ src/main/java/io/robusta/rra/cache/MyCache.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2014. by Robusta Code and individual contributors
+ *  as indicated by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.robusta.rra.cache;
 
 import io.robusta.rra.representation.Representation;
@@ -27,8 +50,10 @@ import java.util.Set;
  * used) entry is dropped.
  * 
  * This class is thread-safe. All methods of this class are synchronized.
+ * 
+ * @author Jean-Marc Villatte
+ *
  */
-
 public class MyCache {
 
     private static MyCache                        instance;
@@ -183,6 +208,13 @@ public class MyCache {
         }
     }
 
+    /**
+     * Retrieve All Fields (inherited) from a Class
+     * 
+     * @param fields
+     * @param type
+     * @return
+     */
     private static List<Field> getAllFields( List<Field> fields, Class<?> type ) {
         for ( Field field : type.getDeclaredFields() ) {
             fields.add( field );
@@ -195,6 +227,13 @@ public class MyCache {
         return fields;
     }
 
+    /**
+     * Retrieve All Fields (inherited) from a Class
+     * 
+     * @param fields
+     * @param type
+     * @return
+     */
     private void mapCacheDependenciesAddEntry( String representationKey, String resourceKey ) {
         if ( !mapCacheDependencies.containsKey( resourceKey ) ) {
             Set<String> set = Collections.synchronizedSet( new
@@ -252,14 +291,25 @@ public class MyCache {
         return new ArrayList<Map.Entry<String, Representation>>( mapCache.entrySet() );
     }
 
+    /**
+     * Returns a <code>ArrayList</code> that contains a copy of all
+     * mapCacheDependencies entries.
+     * 
+     * @return a <code>ArrayList</code> with a copy of the mapCacheDependencies
+     *         content.
+     */
     public synchronized ArrayList<Entry<String, Set<String>>> getAllDependenciesMap() {
         return new ArrayList<Map.Entry<String, Set<String>>>( mapCacheDependencies.entrySet() );
     }
 
+    /**
+     * Display cache and mapCacheDependencies content
+     * 
+     */
     public void displayCache() {
-        System.out.println( "*** MyCache: Contenu du cache***" );
+        System.out.println( "*** MyCache content ***" );
         System.out.println( getAll() );
-        System.out.println( "***Contenu de dependencie map***" );
+        System.out.println( "***dependencie map content ***" );
         System.out.println( getAllDependenciesMap() );
         System.out.println( "----------------" );
     }
--- src/main/java/io/robusta/rra/cache/RraCache.java
+++ src/main/java/io/robusta/rra/cache/RraCache.java
@@ -1,15 +1,51 @@
+/*
+ * Copyright (c) 2014. by Robusta Code and individual contributors
+ *  as indicated by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.robusta.rra.cache;
 
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.Element;
 
+/**
+ * A cache, based on <code>Ehcache</code>.
+ * 
+ * 
+ * @author Jean-Marc Villatte
+ *
+ */
+
 public class RraCache {
 
     private static RraCache instance;
-
     private static Ehcache  cache;
 
+    /**
+     * Retrieve the cache named cacheName
+     * 
+     * @param cacheName
+     * @return Ehcache
+     */
     public static Ehcache getCache( String cacheName ) {
         if ( instance == null ) {
             instance = new RraCache( cacheName );
@@ -26,11 +62,16 @@ public class RraCache {
         return instance;
     }
 
+    /**
+     * Display cache content
+     * 
+     */
     public void displayCache() {
-        System.out.println( "*** RraCache: Contenu du cache***" );
+        System.out.println( "*** RraCache content***" );
         for ( Element element : cache.getAll( cache.getKeys() ).values() ) {
             System.out.println( element );
         }
         System.out.println( "----------------" );
     }
+
 }
